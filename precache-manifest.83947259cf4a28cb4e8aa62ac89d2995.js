18e6c1de11ef3cdbb816f3d6fe44b8c8bfacefd0
Håkon Paulsen <hakon.r.paulsen@accenture.com>
2020-10-08 16:43:21 +0200

    Updates

---
+++ precache-manifest.83947259cf4a28cb4e8aa62ac89d2995.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3d082b64949c37bf4437b9d3e637df",
+    "url": "/random-pokemon-generator/index.html"
+  },
+  {
+    "revision": "58398e68868ac038b146",
+    "url": "/random-pokemon-generator/static/css/main.4d976916.chunk.css"
+  },
+  {
+    "revision": "19e9f4188d45d21ee38b",
+    "url": "/random-pokemon-generator/static/js/2.b7ac5c46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-pokemon-generator/static/js/2.b7ac5c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58398e68868ac038b146",
+    "url": "/random-pokemon-generator/static/js/main.3238bf5e.chunk.js"
+  },
+  {
+    "revision": "53e2108123cf1b45be2e",
+    "url": "/random-pokemon-generator/static/js/runtime-main.a29a02fe.js"
+  },
+  {
+    "revision": "7aa525cedf8bdc6aa2a356e85e63d266",
+    "url": "/random-pokemon-generator/static/media/FuturaLT-Bold.7aa525ce.woff"
+  },
+  {
+    "revision": "1c0b45e2727640f0436b0af01920ed2b",
+    "url": "/random-pokemon-generator/static/media/FuturaLT-Condensed.1c0b45e2.woff"
+  },
+  {
+    "revision": "125b07aa589a08d43a3296c72b070639",
+    "url": "/random-pokemon-generator/static/media/FuturaLT-CondensedBold.125b07aa.woff"
+  },
+  {
+    "revision": "cba022b0348b08e5bc630d0bf7fcb1aa",
+    "url": "/random-pokemon-generator/static/media/FuturaLT.cba022b0.woff"
+  },
+  {
+    "revision": "01d042f0f2845880c97c4e2d77ff3c53",
+    "url": "/random-pokemon-generator/static/media/GillSansStd.01d042f0.woff"
+  },
+  {
+    "revision": "82520c5be1b68d2bbcafcfea399aacc3",
+    "url": "/random-pokemon-generator/static/media/GillSansStdBold.82520c5b.woff"
+  },
+  {
+    "revision": "562ea2b7bd6d408f64361a4800c51efe",
+    "url": "/random-pokemon-generator/static/media/GillSansStdBoldCondensed.562ea2b7.woff"
+  },
+  {
+    "revision": "5614cdbac44db763d06af42c23bcd846",
+    "url": "/random-pokemon-generator/static/media/GillSansStdCondensed.5614cdba.woff"
+  },
+  {
+    "revision": "855f5afdb5b074e3d16d6ae5127bcab6",
+    "url": "/random-pokemon-generator/static/media/GillSansStdExtraBold.855f5afd.woff"
+  },
+  {
+    "revision": "12491af1636693672ac411d03366e2fe",
+    "url": "/random-pokemon-generator/static/media/GillSansStdLight.12491af1.woff"
+  },
+  {
+    "revision": "2de64a23060cf5dac90647957828ccdb",
+    "url": "/random-pokemon-generator/static/media/dragon_0.2de64a23.png"
+  },
+  {
+    "revision": "3de1e62fbd616ec1a4f3f9376edc8c05",
+    "url": "/random-pokemon-generator/static/media/dragon_1.3de1e62f.png"
+  },
+  {
+    "revision": "72aae9b09f8f92805ff724294899c718",
+    "url": "/random-pokemon-generator/static/media/dragon_2.72aae9b0.png"
+  },
+  {
+    "revision": "7eee378c4ab6b42620ba78d50fc94411",
+    "url": "/random-pokemon-generator/static/media/electric.7eee378c.png"
+  },
+  {
+    "revision": "99429d7c47252cdab8a550ad226a1c0e",
+    "url": "/random-pokemon-generator/static/media/electric_0.99429d7c.png"
+  },
+  {
+    "revision": "3650773780bc496781e51dcd44e9175e",
+    "url": "/random-pokemon-generator/static/media/electric_1.36507737.png"
+  },
+  {
+    "revision": "4b3bc754c4bc8a8baf90e8f918f1c1c8",
+    "url": "/random-pokemon-generator/static/media/electric_2.4b3bc754.png"
+  },
+  {
+    "revision": "37fe67f5211c51863efa41d6546ae36f",
+    "url": "/random-pokemon-generator/static/media/fighting.37fe67f5.png"
+  },
+  {
+    "revision": "6e76f28412ebc2fdf51c69c457d9144f",
+    "url": "/random-pokemon-generator/static/media/fighting_0.6e76f284.png"
+  },
+  {
+    "revision": "a0f7fc414dc9068fb964ced83239a498",
+    "url": "/random-pokemon-generator/static/media/fighting_1.a0f7fc41.png"
+  },
+  {
+    "revision": "0069788b01ee2cf804a455df98c52914",
+    "url": "/random-pokemon-generator/static/media/fighting_2.0069788b.png"
+  },
+  {
+    "revision": "52953182f4f2087c38ada07cd47b4ad0",
+    "url": "/random-pokemon-generator/static/media/fire.52953182.png"
+  },
+  {
+    "revision": "bfc276559e9730cb59282bab78ddbae0",
+    "url": "/random-pokemon-generator/static/media/fire_0.bfc27655.png"
+  },
+  {
+    "revision": "974f182d6c1dff1660528a9cb9e757a7",
+    "url": "/random-pokemon-generator/static/media/fire_1.974f182d.png"
+  },
+  {
+    "revision": "8ec240f14d8bdfbb9269ea94bf3abe80",
+    "url": "/random-pokemon-generator/static/media/fire_2.8ec240f1.png"
+  },
+  {
+    "revision": "9e3a8dd0689db16e38e51d363ca54d9f",
+    "url": "/random-pokemon-generator/static/media/grass.9e3a8dd0.png"
+  },
+  {
+    "revision": "90135cfeb19278de7574467b70ebde54",
+    "url": "/random-pokemon-generator/static/media/grass_0.90135cfe.png"
+  },
+  {
+    "revision": "27d05f1578bb7d5722b9562a6b102f68",
+    "url": "/random-pokemon-generator/static/media/grass_1.27d05f15.png"
+  },
+  {
+    "revision": "10f4df669801d3b90df5b3c947266e0f",
+    "url": "/random-pokemon-generator/static/media/grass_2.10f4df66.png"
+  },
+  {
+    "revision": "39f8ae639aba77066af11f8b554fcf98",
+    "url": "/random-pokemon-generator/static/media/logo.39f8ae63.svg"
+  },
+  {
+    "revision": "a1162492af83bb4cce3f6da43dc724e1",
+    "url": "/random-pokemon-generator/static/media/normal.a1162492.png"
+  },
+  {
+    "revision": "1d9e2ef77107709ec179b7401a9daa0b",
+    "url": "/random-pokemon-generator/static/media/normal_0.1d9e2ef7.png"
+  },
+  {
+    "revision": "a9d0f9d6b21a39355292eb85ee36182c",
+    "url": "/random-pokemon-generator/static/media/normal_1.a9d0f9d6.png"
+  },
+  {
+    "revision": "8394f70521cd7fe731efe282fd3bb13f",
+    "url": "/random-pokemon-generator/static/media/normal_2.8394f705.png"
+  },
+  {
+    "revision": "2b4798f5c303bb0d9001413af970a22b",
+    "url": "/random-pokemon-generator/static/media/pokeball.2b4798f5.svg"
+  },
+  {
+    "revision": "33d9204b79416a18454852b0964df6a4",
+    "url": "/random-pokemon-generator/static/media/psychic.33d9204b.png"
+  },
+  {
+    "revision": "5a87b50bfa33012df73e9369aa9af3d3",
+    "url": "/random-pokemon-generator/static/media/psychic_0.5a87b50b.png"
+  },
+  {
+    "revision": "7c0feb742c99ac92d76360a0e0ee3d3b",
+    "url": "/random-pokemon-generator/static/media/psychic_1.7c0feb74.png"
+  },
+  {
+    "revision": "af5e9fcaf2494b9016f04cdf00433433",
+    "url": "/random-pokemon-generator/static/media/psychic_2.af5e9fca.png"
+  },
+  {
+    "revision": "1d3035bc65ea7a261f8df2bd22631ab9",
+    "url": "/random-pokemon-generator/static/media/water.1d3035bc.png"
+  },
+  {
+    "revision": "0cfc0b9166da1f2596c212c2591ddd0e",
+    "url": "/random-pokemon-generator/static/media/water_0.0cfc0b91.png"
+  },
+  {
+    "revision": "c972a5992714005846e1124d04a41c57",
+    "url": "/random-pokemon-generator/static/media/water_1.c972a599.png"
+  },
+  {
+    "revision": "3c988f6d67a1c56e589a3194be097525",
+    "url": "/random-pokemon-generator/static/media/water_2.3c988f6d.png"
+  }
+]);
